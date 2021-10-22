b3bdf4ad862fb870277cfe15fc49358b8678446c
Ephraim Bundi <ebundi04@gmail.com>
2021-10-22 10:51:41 +0300

    Initial commit

--- insta/urls.py
+++ insta/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
 from django.conf.urls import url,include
 from django.contrib.auth import views
 
@@ -22,7 +22,7 @@ from django.contrib.auth import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     url(r'^',include('instagram.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.LogoutView.as_view(), {"next_page": '/'}),
